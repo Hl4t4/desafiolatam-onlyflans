1a575e420b7a6a16bcb31fcbb347a260b82bc985
Jose Latapiatt <jose.latapiatt@proton.me>
2024-04-25 18:20:15 -0400

    Hito 3 stilying

--- onlyflans/web/forms.py
+++ onlyflans/web/forms.py
@@ -10,3 +10,8 @@ class ContactFormModelForm(forms.ModelForm):
     class Meta:
         model = ContactForm
         fields = ['customer_email', 'customer_name', 'message']
+        widgets = {
+            'customer_email' : forms.EmailInput(attrs={'class':'form-control', 'placeholder':'correo@dominio.cl'}),
+            'customer_name' : forms.TextInput(attrs={'class':'form-control', 'placeholder':'Nombre'}),
+            'message' : forms.Textarea(attrs={'class':'form-control'}),
+        }
